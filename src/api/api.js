cd449d0763b3e305e78c4f57596c3387f62f9765
hwangjihong <wlghd6737@gmail.com>
2024-11-05 11:13:52 +0900

    v0.1

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,9 @@ import axios from "axios";
 // 선택된 학년 데이터 가져오기
 export const getStudentByGrade = async (grade, setSubjectsData) => {
   try {
-    const res = await axios.get(`http://localhost:4000/students/${grade}`);
+    const res = await axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/students/${grade}`
+    );
     setSubjectsData(res.data);
   } catch (err) {
     console.error(err);
@@ -13,8 +15,22 @@ export const getStudentByGrade = async (grade, setSubjectsData) => {
 // 새로 입력 받은 과목 데이터 저장
 export const saveSubjectsData = async (grade, subjectsData) => {
   try {
-    await axios.put(`http://localhost:4000/students/${grade}`, subjectsData);
+    await axios.put(
+      `${process.env.REACT_APP_SERVER_URL}/${grade}`,
+      subjectsData
+    );
     alert("저장 완료!!");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getTest = async (grade, setTest) => {
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/${grade + "year"}`
+    );
+    setTest(res.data);
   } catch (err) {
     console.error(err);
   }
